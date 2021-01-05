f3207c052233f97f9758f7c54e070830535242b5
tmddkjung <tmddkjung@naver.com>
2021-01-05 14:53:37 +0900

    set home form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+
+    const [nweet, setNweet] = useState("")
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
+    const onChange = (event) => {
+        const { target : { value }} = event;
+        setNweet(value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type={"text"}
+                       placeholder={"What's on your mind?"}
+                       maxLength={120}
+                       onChange={onChange}
+                />
+                <input type={"submit"} value={"nweet"}/>
+            </form>
+        </div>
+    )
+}
 
 export default Home;
