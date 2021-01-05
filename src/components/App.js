cd1945f9680dd88d43772128fb7fab9c6f8d8e9c
tmddkjung <tmddkjung@naver.com>
2021-01-05 11:54:50 +0900

    Sign in or Create new account

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+    const [init, setInit] = useState(false);
 
-  return (
-      <>
-        <AppRouter isLoggedIn={isLoggedIn}/>
-        <footer>&copy; Nwitter</footer>
-      </>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()=>{
+        authService.onAuthStateChanged((user)=>{
+            if(user){
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false);
+            }
+            setInit(true)
+        })
+    }, [])
+
+    return (
+        <>
+            {
+                init
+                    ? <AppRouter isLoggedIn={isLoggedIn}/>
+                    : "Initializing..."
+            }
+            <footer>&copy; Nwitter</footer>
+        </>
+    );
 }
 
 export default App;
