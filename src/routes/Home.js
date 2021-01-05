9b6909199a65be386cc10927892bffa1d7e094b6
tmddkjung <tmddkjung@naver.com>
2021-01-05 15:28:39 +0900

    get and display nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
-
     const [nweet, setNweet] = useState("")
+    const [nweets, setNweets] = useState([])
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+
+        dbNweets.forEach((document)=>{
+            const nweetObj = {
+                ...document.data(),
+                id: document.id
+            }
+
+            setNweets(prev=>[nweetObj, ...prev])
+        })
+    }
+
+    useEffect(()=>{
+        getNweets();
+    }, []);
+
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -27,6 +45,16 @@ const Home = () => {
                 />
                 <input type={"submit"} value={"nweet"}/>
             </form>
+            <div>
+                {
+                    nweets.map((comment)=>{
+                        return <div key={comment.id}>
+                            <span>{comment.nweet}</span>
+                            <span>{comment.createdAt}</span>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
